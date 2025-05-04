c174743813e231e658810c0c64ccf17ced9fd37d
Елена <99683487+eln02@users.noreply.github.com>
2025-05-04 12:32:28 +0300

    fix

--- lib/src/features/content/ui/widgets/route_card.dart
+++ lib/src/features/content/ui/widgets/route_card.dart
@@ -64,31 +64,6 @@ class _CardBodyWidget extends StatelessWidget {
             SizedBox(
               height: 12,
             ),
-            Row(
-              children: [
-                _LikeButton(initialCount: 12),
-                const SizedBox(width: 12),
-                _CommentButton(
-                  count: 3,
-                  onTap: () {},
-                ),
-                Spacer(),
-                AppElevatedButton.main(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.bookmark_border,
-                          size: 24,
-                        ),
-                        SizedBox(
-                          width: 6,
-                        ),
-                        Text('Сохранить')
-                      ],
-                    ),
-                    onPressed: () {})
-              ],
-            ),
             _RouteDeeplinksButtons(
               route: route,
             )
@@ -256,81 +231,7 @@ class _DescriptionTextState extends State<_DescriptionText> {
   }
 }
 
-class _LikeButton extends StatefulWidget {
-  final int initialCount;
 
-  const _LikeButton({
-    required this.initialCount,
-  });
-
-  @override
-  State<_LikeButton> createState() => _LikeButtonState();
-}
-
-class _LikeButtonState extends State<_LikeButton> {
-  bool isLiked = false;
-  late int likeCount;
-
-  @override
-  void initState() {
-    super.initState();
-    likeCount = widget.initialCount;
-  }
-
-  void _toggleLike() {
-    setState(() {
-      isLiked = !isLiked;
-      likeCount += isLiked ? 1 : -1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AppElevatedButton.minor(
-      child: Row(
-        children: [
-          if (isLiked)
-            SvgPicture.asset(AppVectors.heartFilled)
-          else
-            SvgPicture.asset(AppVectors.heartOutlined),
-          SizedBox(
-            width: 6,
-          ),
-          Text('$likeCount')
-        ],
-      ),
-      onPressed: _toggleLike,
-    );
-  }
-}
-
-class _CommentButton extends StatelessWidget {
-  final int count;
-  final VoidCallback onTap;
-
-  const _CommentButton({
-    required this.count,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return AppElevatedButton.minor(
-      child: Row(
-        children: [
-          SvgPicture.asset(AppVectors.comment),
-          SizedBox(
-            width: 6,
-          ),
-          Text(
-            '$count',
-          ),
-        ],
-      ),
-      onPressed: onTap,
-    );
-  }
-}
 
 class _PostHeader extends StatelessWidget {
   final String username;
